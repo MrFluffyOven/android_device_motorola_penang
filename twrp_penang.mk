f0f670375f3c48a8e2c23adbfee188c6eeaa480e
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-11-13 11:05:24 +0000

    cleanup

--- twrp_penang.mk
+++ twrp_penang.mk
@@ -7,19 +7,16 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from penang device
 $(call inherit-product, device/motorola/penang/device.mk)
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := penang
@@ -29,5 +26,3 @@ PRODUCT_NAME := twrp_penang
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(53)
 PRODUCT_MANUFACTURER := motorola
-
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
