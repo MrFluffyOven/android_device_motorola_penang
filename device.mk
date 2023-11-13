9efa7c26454d9e2ed774dbe187fde5c2a71ee477
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-11-13 11:10:30 +0000

    Cleanup

--- device.mk
+++ device.mk
@@ -4,14 +4,33 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# define hardware platform
+PRODUCT_PLATFORM := penang
 
 LOCAL_PATH := device/motorola/penang
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+
+    # A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor \
+    product \
+    vbmeta \
+    vbmeta_system \
+    dtbo
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
@@ -34,4 +53,29 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
-LOCAL_PATH := device/motorola/penang
+    # tell update_engine to not change dynamic partition table during updates
+# needed since our qti_dynamic_partitions does not include
+# vendor and odm and we also dont want to AB update them
+TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+
+    # Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Encryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Blacklist
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.bootimage.build.date.utc \
+    ro.build.date.utc
