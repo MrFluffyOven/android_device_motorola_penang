015e310d8a31395e5fc75081e08cf9637e977a36
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-11-13 11:03:12 +0000

    Cleanup

--- twrp_penang.mk
+++ twrp_penang.mk
@@ -9,21 +9,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from penang device
 $(call inherit-product, device/motorola/penang/device.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := penang
+
 PRODUCT_DEVICE := penang
 PRODUCT_NAME := twrp_penang
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := Holi for arm64
+PRODUCT_MODEL := moto g(53)
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="penang_g-user 11 T1TP33.75-96-1 7c777 release-keys"
-
-BUILD_FINGERPRINT := motorola/penang_g/penang:11/T1TP33.75-96-1/7c777:user/release-keys
