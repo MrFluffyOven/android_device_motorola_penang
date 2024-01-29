1fe12fe5b4f13bb9e3bd1a40c81801bdfd02aa42
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-01-29 14:25:49 +0000

    Update UpsideDownCake

--- twrp_penang.mk
+++ twrp_penang.mk
@@ -1,24 +1,29 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := penang
+# Inherit from penang device
+$(call inherit-product, device/motorola/penang/device.mk)
 
 PRODUCT_DEVICE := penang
 PRODUCT_NAME := twrp_penang
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g(53)
+PRODUCT_MODEL := Holi for arm64
 PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="penang_g-user 11 U1TP34.29-37-2 666ec release-keys"
+
+BUILD_FINGERPRINT := motorola/penang_g/penang:11/U1TP34.29-37-2/666ec:user/release-keys
